cfc9538a5209dbac12180c326b015d7e13f5d46d
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-17 13:55:14 -0500

    progress

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -49,25 +49,6 @@ router.post("/login", (req, res, next) => {
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
   */
   let { username, password } = req.body;
   if (!username || !username.trim() || !password || !password.trim()) {
