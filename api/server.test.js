6817b76d2b635352751c4ba9a2fb03dc0d263e36
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-17 18:00:11 -0500

    progress on tests

--- api/server.test.js
+++ api/server.test.js
@@ -20,73 +20,67 @@ it("is the correct env", () => {
 });
 
 describe("Auth router", () => {
+  const credentials = { username: "gabe", password: "password123" };
+
   describe("[POST] Register", () => {
+    const endpoint = "/api/auth/register";
+
     it("can register a new user with username & password", async () => {
-      const res = await request(server)
-        .post("/api/auth/register")
-        .send({ username: "gabe", password: "password123" });
+      const res = await request(server).post(endpoint).send(credentials);
       expect(res.status).toBe(201);
     });
     it("will not allow duplicate usernames", async () => {
-      await request(server)
-        .post("/api/auth/register")
-        .send({ username: "gabe", password: "password123" });
-      const res = await request(server)
-        .post("/api/auth/register")
-        .send({ username: "gabe", password: "password123" });
+      await request(server).post(endpoint).send(credentials);
+      const res = await request(server).post(endpoint).send(credentials);
       expect(res.body.message).toBe("username taken");
     });
     it("error when missing username", async () => {
       const res = await request(server)
-        .post("/api/auth/register")
-        .send({ password: "password123" });
+        .post(endpoint)
+        .send(credentials.password);
       expect(res.body.message).toBe("username and password required");
+      expect(res.body.status).toBe(400);
     });
     it("error when missing password", async () => {
       const res = await request(server)
-        .post("/api/auth/register")
-        .send({ username: "gabe" });
+        .post(endpoint)
+        .send(credentials.username);
       expect(res.body.message).toBe("username and password required");
     });
   });
 
   describe("[POST] Login", () => {
-    
-    beforeEach( async () => {
-      await request(server)
-        .post("/api/auth/register")
-        .send({ username: "gabe", password: "password123" });
-    })
+    const endpoint = "/api/auth/login";
+
+    beforeEach(async () => {
+      await request(server).post("/api/auth/register").send(credentials);
+    });
 
     it("can login with username & password", async () => {
-      const res = await request(server)
-        .post("/api/auth/login")
-        .send({ username: "gabe", password: "password123" });
-      expect(res.body.message).toBe(`welcome, gabe`);
+      const res = await request(server).post(endpoint).send(credentials);
+      expect(res.body.message).toBe(`welcome, ${credentials.username}`);
     });
 
     it("error when wrong credentials", async () => {
       const res = await request(server)
-      .post("/api/auth/login")
-      .send({ username: "gabe", password: "badpassword" });
-    expect(res.body.message).toBe(`invalid credentials`);
+        .post(endpoint)
+        .send({ username: "gabe", password: "badpassword" });
+      expect(res.body.message).toBe(`invalid credentials`);
     });
   });
 });
 
 describe("Jokes router", () => {
   describe("[GET] Jokes", () => {
-    // let res;
-    // beforeEach(async () => {
-    //   res = await request(server).get("/api/jokes");
-    // });
+  const endpoint = "/api/jokes"
+    
     it("cannot get jokes without token", async () => {
-      const res = await request(server).get("/api/jokes");
+      const res = await request(server).get(endpoint);
       expect(res.status).toBe(401);
     });
     it("returns message if token is invalid", async () => {
       const res = await await request(server)
-        .get("/api/jokes")
+        .get(endpoint)
         .set("Authorization", "token");
       expect(res.message).toBe("token invalid");
     });
