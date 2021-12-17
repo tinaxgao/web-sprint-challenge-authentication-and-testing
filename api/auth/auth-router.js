38eb9b0a2b6232173b1d1352b2b9a5914e107001
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-17 13:11:32 -0500

    login

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,12 +1,11 @@
-const router = require('express').Router();
-const { JWT_SECRET } = require('../secrets')
+const router = require("express").Router();
+const { JWT_SECRET } = require("../secrets");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const Users = require('../users/users-model')
+const Users = require("../users/users-model");
 
-router.post('/register', (req, res, next) => {
-  
+router.post("/register", (req, res, next) => {
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -33,22 +32,20 @@ router.post('/register', (req, res, next) => {
       the response body should include a string exactly as follows: "username taken".
   */
 
-      const { username, password } = req.body;
-      const hash = bcryptjs.hashSync(password, 8);
-      Users.add({ username, password: hash })
-        .then((newUser) => {
-          res.status(201).json({
-            id: newUser.id,
-            username: newUser.username,
-            password: newUser.password,
-          });
-        })
-        .catch(next);
-
+  const { username, password } = req.body;
+  const hash = bcryptjs.hashSync(password, 8);
+  Users.add({ username, password: hash })
+    .then((newUser) => {
+      res.status(201).json({
+        id: newUser.id,
+        username: newUser.username,
+        password: newUser.password,
+      });
+    })
+    .catch(next);
 });
 
-router.post('/login', (req, res, next) => {
- 
+router.post("/login", (req, res, next) => {
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -72,23 +69,24 @@ router.post('/login', (req, res, next) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
-      let { username, password } = req.body
+  let { username, password } = req.body;
+  if (!username || !username.trim() || !password || !password.trim()) {
+    res.json({ message: "username and password required" });
+  }
 
-      Users.findBy({ username })
-      .then(([user]) => {
-        if (user && bcryptjs.compareSync(password, user.password)) {
-      
-          const token = buildToken(user)
-          res.status(200).json({
-            message: `Welcome back ${user.username}...`,
-            token,
-          })
-        } else {
-          next({ status: 401, message: 'Invalid Credentials' })
-        }
-      })
-      .catch(next)
-
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user && bcryptjs.compareSync(password, user.password)) {
+        const token = buildToken(user);
+        res.status(200).json({
+          message: `welcome, ${user.username}`,
+          token,
+        });
+      } else {
+        next({ status: 401, message: "invalid credentials" });
+      }
+    })
+    .catch(next);
 });
 
 function buildToken(user) {
